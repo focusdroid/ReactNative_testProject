4e6c4f40680b63bc4b835ea686d977e261f91e00
focusdroid <840254112@qq.com>
2019-10-07 18:49:14 +0800

    动态配置顶部tab，和传参

--- src/pages/PopularPage.js
+++ src/pages/PopularPage.js
@@ -5,26 +5,41 @@ import {
 } from 'react-navigation';
 import {
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native';
 import NavigationUtil from '../navigator/NavigationUtil';
 
 
 /*最热页面*/
 export default class PopularPage extends Component {
+  constructor(props){
+    super(props);
+    this.tabNames=['Java', 'Android', 'PHP', 'IOS', 'JavaScript', 'React', 'ReactNative']
+  }
+  genTabs () {
+    const tabs = {}
+    this.tabNames.forEach((item, i)=>{
+      tabs[`tab${i}`] = {
+        screen: props => <PopularTab {...props} tabLabel={item}/>, // 动态路由传参
+        navigationOptions: {
+          title: item
+      }
+      }
+    })
+    return tabs
+  }
   render() {
-    const TabNavigator = createAppContainer(createMaterialTopTabNavigator({
-      PopularTab1: {
-        screen: PopularTab,
-        navigationOptions: {
-          title: 'tab1'
-        }
-      },
-      PopularTab2: {
-        screen: PopularTab,
-        navigationOptions: {
-          title: 'tab2'
-        }
+    const TabNavigator = createAppContainer(createMaterialTopTabNavigator(this.genTabs(), {
+      tabBarOptions: {
+        tabStyle: styles.tabStyle,
+        upperCaseLabel: false, // 标签是都全是大写，默认是true
+        scrollEnabled: true, // 是否支持滚动，默认是false
+        style: {
+          backgroundColor: '#678' // tabBar的背景颜色
+        },
+        indicatorStyle: styles.indicatorStyle, // 标签指示器的样式， 地下切换的那个条
+        labelStyle: styles.labelStyle, // 文字样式
       }
     }))
     return (<TabNavigator/>);
@@ -42,3 +57,18 @@ class PopularTab extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  tabStyle: {
+    minWidth: 50
+  },
+  indicatorStyle: {
+    height: 2,
+    backgroundColor: '#fff'
+  },
+  labelStyle: {
+    fontSize: 13,
+    marginTop: 6,
+    marginBottom: 6
+  }
+})
